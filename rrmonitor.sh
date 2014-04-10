def45c88f5baae9bd6afc29d870d43083b9206e9
Gerhard <info@tinned-software.net>
2014-04-10 11:47:10 +0200

    increased version number for recent changes

--- rrmonitor.sh
+++ rrmonitor.sh
@@ -2,7 +2,7 @@
 #
 # @author Gerhard Steinbeis (info [at] tinned-software [dot] net)
 # @copyright Copyright (c) 2014
-version=0.4.5
+version=0.4.6
 # @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3
 # @package monitoring
 #
