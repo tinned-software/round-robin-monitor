136c6366c67e85a6e14800987a91429a40a113db
Gerhard <info@tinned-software.net>
2014-04-25 15:02:53 +0200

    changed DS-name in rrd databases to the IP address

--- rrmonitor_rrdtool.sh
+++ rrmonitor_rrdtool.sh
@@ -2,7 +2,7 @@
 #
 # @author Gerhard Steinbeis (info [at] tinned-software [dot] net)
 # @copyright Copyright (c) 2014
-version=0.1.0
+version=0.1.1
 # @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3
 # @package monitoring
 #
@@ -15,38 +15,50 @@ fi
 
 function rrdtool_create
 {
+	#echo "started rrdtool_create $@"
+
 	# Get the parameter 1, the IP_ADDRESS
 	IP_ADDRESS=$1
+	IP_NAME=`echo "$IP_ADDRESS" | sed 's/[:\.]/_/g'`
 
 	# get the start time for generating the db
 	START_TIME=`date +%s`
+	START_TIME=$((START_TIME-$RRDTOOL_STEP_TIME))
 
 	# create RRD with an entry every 5 monites and a everage 
-	$RRDTOOL_CMD create ${RRDTOOL_DBPATH}${IP_ADDRESS}.rrd --start $START_TIME --step=$RRDTOOL_STEP_TIME \
-		DS:value:GAUGE:$RRDTOOL_STEP_TIME_MISSING:U:U \
-		RRA:AVERAGE:0.5:1:$RRDTOOL_ENTRIES}
+	$RRDTOOL_CMD create ${RRDTOOL_DBPATH}${IP_NAME}.rrd --start $START_TIME --step=$RRDTOOL_STEP_TIME \
+		DS:$IP_NAME:GAUGE:$RRDTOOL_STEP_TIME_MISSING:U:U \
+		RRA:AVERAGE:0.5:1:$RRDTOOL_ENTRIES
 }
 
 
 
 function rrdtool_update
 {
+	#echo "started rrdtool_update $@"
+
 	# Get the time of the data set
 	CURRENT_TIME=$1
 
-	# Get the parameter 1, the IP_ADDRESS
+	# Get the parameter 2, the IP_ADDRESS
 	IP_ADDRESS=$2
+	IP_NAME=`echo "$IP_ADDRESS" | sed 's/[:\.]/_/g'`
 
-	# Get the parameter 2, the VALUE
+	# Get the parameter 3, the VALUE
 	VALUE=$3
-
-	$RRDTOOL_CMD update ${RRDTOOL_DBPATH}${IP_ADDRESS}.rrd $CURRENT_TIME:${VALUE}
+	if [ ! -f "${RRDTOOL_DBPATH}${IP_NAME}.rrd" ]
+	then
+		rrdtool_create $IP_ADDRESS
+	fi
+	$RRDTOOL_CMD update ${RRDTOOL_DBPATH}${IP_NAME}.rrd $CURRENT_TIME:${VALUE}
 }
 
 
 
 function rrdtool_graph
 {
+	#echo "started rrdtool_graph $@"
+
 	# get parameters as array (IP LIST)
 	IP_LIST=(`echo "$@"  | tr ' ' '\n' | sort`)
 
@@ -58,10 +70,11 @@ function rrdtool_graph
 	LINE=""
 	for (( i=0; i<${#IP_LIST[@]}; i++ ))
 	do
+		IP_NAME=`echo "${IP_LIST[$i]}" | sed 's/[:\.]/_/g'`
 		REG=${IP_LIST[$i]}
 		REG_SPECIAL=`echo "$REG" | sed 's/\./_/g'`
 		COLOR=${RRDTOOL_GRAPH_COLORS[$i]}
-		LINE="$LINE DEF:$REG_SPECIAL=${RRDTOOL_DBPATH}$REG.rrd:value:AVERAGE LINE:$REG_SPECIAL#$COLOR:$REG "
+		LINE="$LINE DEF:$REG_SPECIAL=${RRDTOOL_DBPATH}$IP_NAME.rrd:$IP_NAME:AVERAGE LINE:$REG_SPECIAL#$COLOR:$REG "
 	done
 
 
